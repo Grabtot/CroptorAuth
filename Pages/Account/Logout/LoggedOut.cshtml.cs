c2e326d3a9a82165539ff99cf731d341c3787011
Grabtot <dneshotkin@gmail.com>
2024-01-17 09:39:11 +0200

    Added post logout redirect

--- Pages/Account/Logout/LoggedOut.cshtml.cs
+++ Pages/Account/Logout/LoggedOut.cshtml.cs
@@ -1,5 +1,6 @@
 using Duende.IdentityServer.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CroptorAuth.Pages.Logout
@@ -17,11 +18,12 @@ namespace CroptorAuth.Pages.Logout
             _interactionService = interactionService;
         }
 
-        public async Task OnGet(string logoutId)
+        public async Task<IActionResult> OnGet(string logoutId)
         {
             // get context information (client name, post logout redirect URI and iframe for federated signout)
             Duende.IdentityServer.Models.LogoutRequest logout = await _interactionService.GetLogoutContextAsync(logoutId);
 
+            return Redirect(logout.PostLogoutRedirectUri ?? "http://localhost:3000");
 
             View = new LoggedOutViewModel
             {
